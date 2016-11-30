ed27abc797204d40e2743087c9ab6cf82d4f59a0
Ivan Dimanov <ivan.dimanov@xogito.com>
2016-11-30 19:03:32 +0200

    DB - create basic DB migration with test data

---
+++ database/index.js
@@ -0,0 +1,86 @@
+'use script'
+
+const fs = require('fs')
+const path = require('path')
+
+const uuid = require('node-uuid')
+const redis = require('redis')
+const bluebird = require('bluebird')
+
+const config = require('../config')
+const logger = require('../shared-modules/logger')
+const {toString} = require('../shared-modules/utils')
+
+/* Used in logging */
+global.serviceName = 'DB Migrate Up'
+global.instanceId = 1
+global.requestId = uuid.v4()
+
+const dbConfig = config.database.redis
+const client = redis.createClient(dbConfig.port, dbConfig.host)
+
+/* Use Promises instead of callbacks: http://redis.js.org/#redis-a-nodejs-redis-client-usage-example-promises */
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+client.on('ready', () => logger.debug(`Redis connection established to ${toString(dbConfig)}`))
+client.on('error', (error) => logger.error(`Error while setting Redis with connection ${toString(dbConfig)}: ${toString(error)}`))
+
+/* Get a descriptive description on
+   - which migrations we have saved in files
+   - which of those are already migrated
+   - which needs to be migrated
+*/
+;(async function main () {
+  const migrationPaths = fs
+    /* Get all possible migration scripts */
+    .readdirSync(path.resolve(__dirname, './migrations'))
+
+    /* Be sure we're dealing only with migration Files */
+    .filter((itemPath) => fs.lstatSync(path.resolve(__dirname, './migrations', itemPath)).isFile())
+
+    /* Secure consistent order of execution */
+    .sort()
+
+  const alreadyCompletedMigrations = await client
+    .lrangeAsync('migrations', 0, -1)
+
+  /* Be sure that all previously executed Migrations are still available as a source */
+  alreadyCompletedMigrations
+    .forEach((itemPath) => {
+      try {
+        fs.lstatSync(path.resolve(__dirname, './migrations', itemPath)).isFile()
+      } catch (error) {
+        throw new ReferenceError(`Cannot access file for already completed Migration "${itemPath}": ${error}`)
+      }
+    })
+
+  const tobeMigratedPaths = migrationPaths
+    .filter((itemPath) => !~alreadyCompletedMigrations.indexOf(itemPath))
+
+  logger.debug('File migrations:', migrationPaths)
+  logger.debug('Already migrated:', alreadyCompletedMigrations)
+  logger.debug('Tobe migrated:', tobeMigratedPaths)
+
+  return tobeMigratedPaths
+})()
+
+/* Start migrating one-by-one the scripts that are missed in the DB */
+.then((tobeMigratedPaths) => Promise.all(tobeMigratedPaths
+  .map((itemPath) => {
+    const migration = require(path.resolve(__dirname, './migrations', itemPath))
+    return migration
+      .up(client)
+      .then(() => {
+        logger.debug('Successfully migrated:', itemPath)
+        /* Be sure not to migrate the same script again */
+        return client.rpushAsync('migrations', itemPath)
+      })
+  }))
+)
+
+.then(() => logger.info('All migrations successfully completed'))
+.catch((error) => logger.error(`Error while executing migrations: ${toString(error)}`))
+
+/* No need to keep the Redis connection open for this module or any other required modules like 'logger' */
+.then(process.exit)
---
+++ database/migrate-down.js
@@ -0,0 +1,72 @@
+'use script'
+
+const fs = require('fs')
+const path = require('path')
+
+const uuid = require('node-uuid')
+const redis = require('redis')
+const bluebird = require('bluebird')
+
+const config = require('../config')
+const logger = require('../shared-modules/logger')
+const {toString} = require('../shared-modules/utils')
+
+/* Used in logging */
+global.serviceName = 'DB Migrate Down'
+global.instanceId = 1
+global.requestId = uuid.v4()
+
+const dbConfig = config.database.redis
+const client = redis.createClient(dbConfig.port, dbConfig.host)
+
+/* Use Promises instead of callbacks: http://redis.js.org/#redis-a-nodejs-redis-client-usage-example-promises */
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+client.on('ready', () => logger.debug(`Redis connection established to ${toString(dbConfig)}`))
+client.on('error', (error) => logger.error(`Error while running Redis with connection ${toString(dbConfig)}: ${toString(error)}`))
+
+const migrationPath = process.argv[2]
+;(async function main () {
+  if (!migrationPath) {
+    throw new ReferenceError(`No migration path found. Example of correct usage "node migrate-down 0-setup.js"`)
+  }
+
+  try {
+    fs.lstatSync(path.resolve(__dirname, './migrations', migrationPath)).isFile()
+  } catch (error) {
+    throw new ReferenceError(`Unable to access migration file "${migrationPath}": ${toString(error)}`)
+  }
+
+  const alreadyCompletedMigrations = await client
+    .lrangeAsync('migrations', 0, -1)
+
+  const migrationIndex = alreadyCompletedMigrations.indexOf(migrationPath)
+  if (!~migrationIndex) {
+    throw new ReferenceError(`File "${migrationPath}" was not migrated into DB ${toString(dbConfig)}`)
+  }
+
+  return alreadyCompletedMigrations
+    .slice(migrationIndex)
+    .reverse()
+})()
+
+/* Start downgrading migrations one-by-one so we can have the DB back to its expected state */
+.then((tobeDowngradedPaths) => Promise.all(tobeDowngradedPaths
+  .map((itemPath) => {
+    const migration = require(path.resolve(__dirname, './migrations', itemPath))
+    return migration
+      .down(client)
+      .then(() => {
+        logger.debug('Successfully downgraded migration:', itemPath)
+        /* Be sure not to downgraded the migrate the same script again */
+        return client.lremAsync('migrations', 0, itemPath)
+      })
+  }))
+)
+
+.then(() => logger.info(`Downgrading to migration "${migrationPath}" successfully completed`))
+.catch((error) => logger.error(`Error in downgrading migration: ${toString(error)}`))
+
+/* No need to keep the Redis connection open */
+.then(process.exit)
