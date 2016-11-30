e3be512ac7e641bab49a6dafece21647575c835f
Ivan Dimanov <ivan.dimanov@xogito.com>
2016-11-30 19:02:49 +0200

    BE - create a Redis-based logger

---
+++ shared-modules/logger/index.js
@@ -0,0 +1,94 @@
+'use strict'
+
+const os = require('os')
+
+const uuid = require('node-uuid')
+const bytes = require('bytes')
+const prettyHrtime = require('pretty-hrtime')
+
+const redis = require('redis')
+const bluebird = require('bluebird')
+
+const config = require('../../config')
+const {toString} = require('../utils')
+
+const dbConfig = config.database.redis
+const client = redis.createClient(dbConfig.port, dbConfig.host)
+
+/* Use Promises instead of callbacks: http://redis.js.org/#redis-a-nodejs-redis-client-usage-example-promises */
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+client.on('error', (error) => logger.error(`Error while setting Redis with connection ${toString(dbConfig)}: ${toString(error)}`))
+
+const serverId = `${os.hostname()} - ${os.type()}/${os.release()}`
+
+async function custom (type, ...messages) {
+  type = toString(type)
+  const message = messages
+    .map(toString)
+    .join(' ')
+
+  const date = new Date()
+  const createdAt = date.toISOString()
+  const log = {
+    type,
+    message,
+    createdAt,
+    serverId,
+    serviceName: global.serviceName,
+    instanceId: global.instanceId,
+    requestId: global.requestId
+  }
+
+  /* Have it in the console as a backup */
+  console.log(createdAt, log)
+
+  /* Keep all logs into sorted set so we can easily paginate or filter by date */
+  await client
+    .zaddAsync('logs', date.getTime(), toString(log))
+}
+
+/* Tracks In & Out requests to a koa server using the local 'logger.*' functions */
+/* Inspired by https://github.com/koajs/logger/blob/master/index.js */
+function * koaMiddleware (next) {
+  const start = process.hrtime()
+
+  global.requestId = this.request.header['x-request-id'] || uuid.v4()
+  this.set('x-request-id', global.requestId)
+
+  logger.info(`<-- ${this.method} ${this.originalUrl}`)
+
+  try {
+    yield next
+  } catch (error) {
+    const length = this.response.length
+    const end = process.hrtime(start)
+    const humanTotalTime = prettyHrtime(end).replace(' ', '')
+
+    /* Log uncaught downstream errors */
+    logger.error(`--> ${this.method} ${this.originalUrl} ${error.status || 500} ${humanTotalTime} ${bytes(length)}`)
+
+    throw error
+  }
+
+  const length = this.response.length
+  const end = process.hrtime(start)
+  const humanTotalTime = prettyHrtime(end).replace(' ', '')
+
+  logger.info(`--> ${this.method} ${this.originalUrl} ${this.status || 500} ${humanTotalTime} ${bytes(length)}`)
+}
+
+const logger = {
+  custom,
+  silly: async (...messages) => await custom('silly', ...messages),
+  debug: async (...messages) => await custom('debug', ...messages),
+  info: async (...messages) => await custom('info', ...messages),
+  warn: async (...messages) => await custom('warning', ...messages),
+  warning: async (...messages) => await custom('warning', ...messages),
+  error: async (...messages) => await custom('error', ...messages),
+
+  koaMiddleware
+}
+
+module.exports = logger
