10d99c9ab9d7f6c85af77044829717d161e6bc2e
Ivan Dimanov <ivan.dimanov@xogito.com>
2016-12-01 13:59:24 +0200

    BE - first Logs mService and related routes

---
+++ back-end/services/logs/routes/system/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const path = require('path')
+
+const koaRouter = require('koa-router')
+const redis = require('redis')
+const bluebird = require('bluebird')
+
+const projectRootPath = '../../../../../'
+const config = require(`${projectRootPath}/config`)
+const logger = require(`${projectRootPath}/shared-modules/logger`)
+const {toString, jsonParseSafe} = require(`${projectRootPath}/shared-modules/utils`)
+
+const routeName = path.parse(__dirname).name
+const apiPrefix = config.services[global.serviceName].routes[routeName].apiPathPrefix
+const urlPrefix = `${apiPrefix}/${global.serviceName}/${routeName}`   /* Resolves to /api/v1/logs/system */
+
+const dbConfig = config.database.redis
+const redisClient = redis.createClient(dbConfig.port, dbConfig.host)
+
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+redisClient.on('error', (error) => console.error(`Error while setting Redis with connection ${toString(dbConfig)}: ${toString(error)}`))
+
+const koaRoutes = koaRouter()
+  .get(urlPrefix, async function () {
+    logger.debug('Getting System logs')
+
+    const logs = await redisClient
+      .zrangeAsync('logs', 0, -1)
+      .map(jsonParseSafe)
+
+    logger.debug('System Logs returned')
+
+    this.body = logs.reverse()
+  })
+
+module.exports = koaRoutes
