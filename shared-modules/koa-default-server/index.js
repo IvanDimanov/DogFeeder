13570734ac5488385ebb2d788070efb891b4eb83
Ivan Dimanov <ivan.dimanov@xogito.com>
2016-12-04 11:56:24 +0200

    BE - have a common Koa server that loads all 'routes'

---
+++ shared-modules/koa-default-server/index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const koa = require('koa')
+const koaBody = require('koa-body')
+
+const config = require('../../config')
+const logger = require('../logger')
+
+const moduleParentDirectory = path.parse(module.parent.filename).dir
+const serviceName = path.parse(moduleParentDirectory).name
+const host = config.services[ serviceName ].host || 'localhost'
+const instancePort = process.env.NODE_APP_INSTANCE | 0
+const basePort = config.services[ serviceName ].defaultPort || 3000
+const port = basePort + instancePort
+
+/* Used in logging */
+global.serviceName = serviceName
+global.instanceId = instancePort
+
+const app = koa()
+
+app
+  .use(logger.koaMiddleware)
+  .use(koaBody())
+
+/* Bind all service Routes from directory './routes' to the current koa 'app' server */
+;(() => fs
+  /* Get all service HTTP routes */
+  .readdirSync(path.resolve(moduleParentDirectory, './routes'))
+
+  /* Be sure we're dealing only with routes Directories */
+  .filter((itemPath) => fs.lstatSync(path.resolve(moduleParentDirectory, './routes', itemPath)).isDirectory())
+
+  /* Secure consistent order of binding */
+  .sort()
+
+  /* Bind each route to the common koa server */
+  .map((itemPath) => {
+    const serviceRoute = require(path.resolve(moduleParentDirectory, './routes', itemPath))
+    app
+      .use(serviceRoute.routes())
+      .use(serviceRoute.allowedMethods())
+  })
+)()
+
+app
+  .listen(port, host, () => logger.info(`Service ${serviceName} running on ${host}:${port}`))
+
+module.exports = app
