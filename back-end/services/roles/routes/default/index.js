c49b2051308d1cd7628956c00359ddd271e7d79e
Ivan Dimanov <ivan.dimanov@xogito.com>
2016-12-06 15:19:20 +0200

    BE - initial roles service

--- back-end/services/roles/routes/default/index.js
+++ back-end/services/roles/routes/default/index.js
@@ -9,11 +9,12 @@ const bluebird = require('bluebird')
 const projectRootPath = '../../../../../'
 const config = require(`${projectRootPath}/config`)
 const logger = require(`${projectRootPath}/shared-modules/logger`)
+const {koaJwtMiddleware} = require(`${projectRootPath}/shared-modules/session`)
 const {toString, jsonParseSafe} = require(`${projectRootPath}/shared-modules/utils`)
 
 const routeName = path.parse(__dirname).name
 const apiPrefix = config.services[global.serviceName].routes[routeName].apiPathPrefix
-const urlPrefix = `${apiPrefix}/${global.serviceName}/${routeName}`
+const urlPrefix = `${apiPrefix}/${global.serviceName}`
 
 const dbConfig = config.database.redis
 const redisClient = redis.createClient(dbConfig.port, dbConfig.host)
@@ -24,19 +25,27 @@ bluebird.promisifyAll(redis.Multi.prototype)
 redisClient.on('error', (error) => console.error(`Error while setting Redis with connection ${toString(dbConfig)}: ${toString(error)}`))
 
 const koaRoutes = koaRouter({
-  /* Resolves to /api/v1/logs/system */
+  /* Resolves to /api/v1/roles */
   prefix: urlPrefix
 })
-  .get('/', function * () {
-    // logger.debug('Getting System logs')
+  /* Let the any system microService know which are the exact roles in use */
+  .get('/internal', koaJwtMiddleware(), function * () {
+    if (!this.state.session.isInternalRequest) {
+      logger.error('Attempt to access internal route with session', this.state.session)
+      this.status = 401
+      this.body = {
+        errorCode: 'InternalUseOnly',
+        errorMessage: 'This route can be used only internally from the system itself'
+      }
+      return
+    }
 
-    const logs = yield redisClient
-      .zrangeAsync('logs', 0, -1)
+    const roles = yield redisClient
+      .lrangeAsync('roles', 0, -1)
       .map(jsonParseSafe)
 
-    // logger.debug('System Logs returned')
-
-    this.body = logs.reverse().slice(0, 50)
+    logger.info('Successfully found found all system roles', roles)
+    this.body = roles
   })
 
 module.exports = koaRoutes
