606c93748830d425269852fc1d317393d241a3f0
Ivan Dimanov <ivan.dimanov@xogito.com>
2017-01-07 18:25:22 +0200

    BE - logs pagination

--- back-end/services/logs/routes/system/index.js
+++ back-end/services/logs/routes/system/index.js
@@ -8,9 +8,8 @@ const bluebird = require('bluebird')
 
 const projectRootPath = '../../../../../'
 const config = require(`${projectRootPath}/config`)
-const logger = require(`${projectRootPath}/shared-modules/logger`)
 const {koaJwtMiddleware} = require(`${projectRootPath}/shared-modules/session`)
-const {toString, jsonParseSafe} = require(`${projectRootPath}/shared-modules/utils`)
+const {toString, getInstance, jsonParseSafe} = require(`${projectRootPath}/shared-modules/utils`)
 
 const routeName = path.parse(__dirname).name
 const apiPrefix = config.services[global.serviceName].routes[routeName].apiPathPrefix
@@ -29,15 +28,56 @@ const koaRoutes = koaRouter({
   prefix: urlPrefix
 })
   .get('/', koaJwtMiddleware(), function * () {
-    // logger.debug('Getting System logs')
+    let {currentPage, maxResultsPerPage} = this.query
+
+    if (currentPage === undefined) {
+      currentPage = 1
+    }
+    currentPage = Number(currentPage)
+
+    if (currentPage % 1) {
+      this.status = 400
+      this.body = {
+        errorCode: 'InvalidPage',
+        errorMessage: `Page must be Integer but same is ${toString(currentPage)}`
+      }
+      return
+    }
+
+    if (currentPage < 1) {
+      this.status = 400
+      this.body = {
+        errorCode: 'InvalidPage',
+        errorMessage: `Page must be bigger than 0 but same is ${toString(currentPage)}`
+      }
+      return
+    }
+
+    if (maxResultsPerPage === undefined) {
+      maxResultsPerPage = 10
+    }
+    maxResultsPerPage = Number(maxResultsPerPage)
+
+    const validMaxResultsPerPage = [5, 10, 20, 30]
+    if (!~validMaxResultsPerPage.indexOf(maxResultsPerPage)) {
+      this.status = 400
+      this.body = {
+        errorCode: 'InvalidMaxResultsPerPage',
+        errorMessage: `Maximum results per page must be one of ${toString(validMaxResultsPerPage)} but same is {${getInstance(maxResultsPerPage)}} "${maxResultsPerPage}"`
+      }
+      return
+    }
 
     const logs = yield redisClient
       .zrangeAsync('logs', 0, -1)
       .map(jsonParseSafe)
 
-    // logger.debug('System Logs returned')
-
-    this.body = logs.reverse().slice(0, 50)
+    this.body = {
+      currentPage,
+      maxResultsPerPage,
+      totalResults: logs.length,
+      logs: logs.reverse().slice((currentPage - 1) * maxResultsPerPage, currentPage * maxResultsPerPage)
+    }
   })
 
 module.exports = koaRoutes
