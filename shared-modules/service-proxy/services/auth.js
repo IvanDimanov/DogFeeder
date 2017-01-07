1dab3d03244f353c8e2fa63438c8291c29bf8c04
Ivan Dimanov <ivan.dimanov@xogito.com>
2017-01-07 15:04:14 +0200

    BE - encryption as mService proxy

--- shared-modules/service-proxy/services/auth.js
+++ shared-modules/service-proxy/services/auth.js
@@ -9,6 +9,24 @@ const defaultPort = defaultProtocol === 'https' ? '' : `:${config['front-end'].p
 const defaultAddress = `${defaultProtocol}://${defaultHost}${defaultPort}`
 
 const proxies = {
+  internalEncrypt (authorizationHeader, text) {
+    return rp({
+      method: 'GET',
+      uri: `${defaultAddress}/api/v1/auth/internal/encrypt`,
+      rejectUnauthorized: false,
+      resolveWithFullResponse: true,
+      json: true,
+      headers: {
+        'x-request-id': global.requestId,
+        Authorization: authorizationHeader
+      },
+      qs: {
+        text
+      }
+    })
+    .then((response) => response.body)
+  },
+
   internalGetSingerByLyrics (authorizationHeader, lyrics) {
     return rp({
       method: 'GET',
