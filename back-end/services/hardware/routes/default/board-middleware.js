ab2015b514fd0361cbd94825ac311686cd5818de
Ivan Dimanov <ivan.dimanov@xogito.com>
2017-04-23 03:54:24 +0300

    BE - move hardware bindings into its API

---
+++ back-end/services/hardware/routes/default/board-middleware.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const exec = require('child_process').exec
+
+const {Board, Led} = require('johnny-five')
+const Raspi = require('raspi-io')
+
+/* Some hardware like Leds needs to have permissions set as GPIO modules */
+;(() => {
+  const gpioModulePaths = ['/sys/class/leds/led0', '/dev/ttyAMA0']
+
+  function bindModule (modulePath) {
+    console.log(`Binding module "${modulePath}"`)
+    const bindCommand = `sudo chmod -R 777 ${modulePath}`
+
+    return new Promise((resolve, reject) => exec(bindCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: Unable to execute "${bindCommand}":\n${error}`)
+        reject(error)
+      } else if (stderr) {
+        console.error(`Error in executing "${bindCommand}"\n${stderr}`)
+        reject(stderr)
+      } else {
+        console.log(`Output for "${bindCommand}":${stdout}`)
+        resolve(stdout)
+      }
+    }))
+  }
+
+  /* Mange all needed permissions for binding GPIO modules */
+  Promise.all(gpioModulePaths.map(bindModule))
+  .then(() => console.log(`All GPIO modules "${gpioModulePaths.join('", "')}" are bound`))
+  .catch((error) => {
+    console.error('Unable to bind GPIO modules because of', error)
+    process.exit(1)
+  })
+})()
+
+/**
+ * Bind all components as soon as the Board is ready
+ * and give access abilities to 'this'
+ */
+const boardSetupMiddleware = (() => {
+  const board = new Board({
+    io: new Raspi(),
+    repl: false,
+    debug: false
+  })
+  let isBoardReady = false
+  let led
+
+  board.on('ready', function onBoardReady () {
+    isBoardReady = true
+    led = new Led('P1-11')
+  })
+
+  return function * boardSetupMiddleware (next) {
+    this.isBoardReady = isBoardReady
+    this.board = board
+    this.led = led
+
+    yield next
+  }
+})()
+
+module.exports = boardSetupMiddleware
--- back-end/services/hardware/routes/default/index.js
+++ back-end/services/hardware/routes/default/index.js
@@ -1,72 +1,19 @@
 'use strict'
 
 const path = require('path')
-const exec = require('child_process').exec
 
 const koaRouter = require('koa-router')
-const {Board, Led} = require('johnny-five')
-const Raspi = require('raspi-io')
 
 const projectRootPath = '../../../../../'
 const config = require(`${projectRootPath}/config`)
-const logger = require(`${projectRootPath}/shared-modules/logger`)
 const {koaJwtMiddleware} = require(`${projectRootPath}/shared-modules/session`)
 
 const routeName = path.parse(__dirname).name
 const apiPrefix = config.services[global.serviceName].routes[routeName].apiPathPrefix
 const urlPrefix = `${apiPrefix}/${global.serviceName}`
 
-/**
- * Bind all components as soon as the Board is ready
- * and give access abilities to 'this'
- */
-const boardSetupMiddleware = (() => {
-  let board
-  let isBoardReady = false
-  let led
-
-  const bindCommand = 'npm run bind-pi-hardware'
-  new Promise((resolve, reject) => exec(bindCommand, {cwd: projectRootPath}, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: Unable to execute "${bindCommand}":\n${error}`)
-      reject(error)
-    } else if (stderr) {
-      console.error(`Error in executing "${bindCommand}"\n${stderr}`)
-      reject(stderr)
-    } else {
-      console.log(`Output for "${bindCommand}":${stdout}`)
-      resolve(stdout)
-    }
-  }))
-
-  .then(() => {
-    logger.debug('Setting up Hardware board access')
-    board = new Board({
-      io: new Raspi(),
-      repl: false,
-      debug: false
-    })
-
-    board.on('ready', function onBoardReady () {
-      logger.info('Hardware board ready')
-      isBoardReady = true
-      led = new Led('P1-11')
-    })
-  })
-
-  .catch((error) => {
-    logger.error('Unable to setup Board', error)
-    throw error
-  })
-
-  return function * boardSetupMiddleware (next) {
-    this.isBoardReady = isBoardReady
-    this.board = board
-    this.led = led
-
-    yield next
-  }
-})()
+/* Handles all GPIO modules that are bound to the Pi Board */
+const boardSetupMiddleware = require('./board-middleware')
 
 const koaRoutes = koaRouter({
   /* Resolves to /api/v1/hardware */
