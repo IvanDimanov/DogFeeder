64f2c56d30ca30c598a73eebffa347a89e678cca
Ivan Dimanov <ivan.dimanov@xogito.com>
2017-07-18 22:41:47 +0300

    BE - use the JS Schema

--- back-end/services/graphql/schema/index.js
+++ back-end/services/graphql/schema/index.js
@@ -1,4 +1,13 @@
-const { makeExecutableSchema } = require('graphql-tools')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLEnumType
+} = require('graphql')
 const moment = require('moment')
 
 const users = [
@@ -67,204 +76,300 @@ function getFilteredClouds ({ ids, name, user = {} }) {
   return result
 }
 
-function getFormatedTime (time, timeFormat) {
-  switch (timeFormat) {
-    case 'MMDDYY':
-      timeFormat = 'MM/DD/YY'
-      break
-    case 'MMDDYYYY':
-      timeFormat = 'MM/DD/YYYY'
-      break
-    case 'DDMMYY':
-      timeFormat = 'DD.MM.YY'
-      break
-    case 'DDMMYYYY':
-      timeFormat = 'DD.MM.YYYY'
-      break
-    default:
-      timeFormat = ''
-  }
+const getFormatedTime = (time, timeFormat) => moment(time).format(timeFormat)
 
-  return moment(time).format(timeFormat)
-}
-
-const typeDefs = `
-  input UserInput {
-    ids: [Int]
-    name: String
-  }
-
-  type User {
-    id: Int!
-    firstName: String
-    lastName: String
-
-    # All the Clouds that this User is Joined
-    clouds(ids: [Int], name: String, user: UserInput): [Cloud]
-
-    # The exact time when the User registered
-    createdAt(timeFormat: TimeFormat = MMDDYY): String!
-
-    # No data ever get deleted from the DB - only marked with a time when it was requested to be deleted
-    deletedAt(timeFormat: TimeFormat = MMDDYY): String
-  }
-
-  # A collection of Users with common goal
-  type Cloud {
-    id: Int!
-    name: String
-
-    # The one and only User that created the Cloud and has permissions over it
-    owner: User
-
-    # All Users Joined to this Cloud
-    users(ids: [Int], name: String): [User]
-
-    # The exact time when the owner User created the Cloud
-    createdAt(timeFormat: TimeFormat = MMDDYY): String!
-
-    # No data ever get deleted from the DB - only marked with a time when it was requested to be deleted
-    deletedAt(timeFormat: TimeFormat = MMDDYY): String
-  }
-
-  # Default Time formatting for all DB time data
-  enum TimeFormat {
-    # Renders "MM/DD/YY"
-    MMDDYY
-    # Renders "MM/DD/YYYY"
-    MMDDYYYY
-    # Renders "DD/MM/YY"
-    DDMMYY
-    # Renders "DD/MM/YYYY"
-    DDMMYYYY
-  }
-
-  # All ways you can Get data from the BE
-  type Query {
-    clouds(ids: [Int], name: String, user: UserInput): [Cloud]
-    users(ids: [Int], name: String): [User]
-  }
-
-  # All ways you can Save data to the BE
-  type Mutation {
-    # Use when a User would like to Join a Cloud
-    joinCloud(userId: Int!, cloudId: Int!): Cloud
-
-    # Use when a User would like to Leave a Cloud
-    leaveCloud(userId: Int!, cloudId: Int!): Cloud
-
-    # Use when the User from JWT would like to Leave a Cloud
-    loggedUserLeaveCloud(cloudId: Int!): Cloud
-  }
-`
-
-const resolvers = {
-  Query: {
-    clouds: (source, args) => getFilteredClouds(args),
-    users: (source, args) => getFilteredUsers(args)
-  },
-
-  Mutation: {
-    joinCloud: (source, { userId, cloudId }) => {
-      const user = users.find((user) => user.id === userId)
-      if (!user) {
-        throw new Error(`Couldn't find User with id '${userId}'`)
-      }
-
-      const cloud = clouds.find((cloud) => cloud.id === cloudId)
-      if (!cloud) {
-        throw new Error(`Couldn't find Cloud with id '${cloudId}'`)
-      }
-
-      if (cloud.usersIds.includes(userId)) {
-        throw new Error(`User with id '${userId}' already joined Cloud with id '${cloudId}'`)
-      }
-
-      cloud.usersIds.push(userId)
-      return new Promise((resolve) => setTimeout(() => resolve(cloud), 3000))
+const TimeFormat = new GraphQLEnumType({
+  name: 'TimeFormat',
+  description: 'Default Time formatting for all DB time data',
+  values: {
+    MMDDYY: {
+      value: 'MM/DD/YY',
+      description: 'Renders "MM/DD/YY"'
     },
-
-    leaveCloud: (source, { userId, cloudId }) => {
-      const user = users.find((user) => user.id === userId)
-      if (!user) {
-        throw new Error(`Couldn't find User with id '${userId}'`)
-      }
-
-      const cloud = clouds.find((cloud) => cloud.id === cloudId)
-      if (!cloud) {
-        throw new Error(`Couldn't find Cloud with id '${cloudId}'`)
-      }
-
-      if (!cloud.usersIds.includes(userId)) {
-        throw new Error(`User with id '${userId}' is not in Cloud with id '${cloudId}'`)
-      }
-
-      cloud.usersIds.splice(cloud.usersIds.indexOf(userId), 1)
-      return new Promise((resolve) => setTimeout(() => resolve(cloud), 3000))
+    MMDDYYYY: {
+      value: 'MM/DD/YYYY',
+      description: 'Renders "MM/DD/YYYY"'
     },
-
-    loggedUserLeaveCloud: (source, { cloudId }, { token }) => {
-      if (!token) {
-        throw new Error(`Couldn't get the Authorization token`)
-      }
-
-      if (token.error) {
-        throw new Error(`Couldn't get the Authorization token - ${token.error}`)
-      }
-
-      const { user } = token
-      if (!user ||
-          !user.id
-        ) {
-        throw new Error(`User in the JWT have no "id" property`)
-      }
-
-      const cloud = clouds.find((cloud) => cloud.id === cloudId)
-      if (!cloud) {
-        throw new Error(`Couldn't find Cloud with id '${cloudId}'`)
-      }
-
-      if (!cloud.usersIds.includes(user.id)) {
-        throw new Error(`User with id '${user.id}' is not in Cloud with id '${cloudId}'`)
-      }
-
-      cloud.usersIds.splice(cloud.usersIds.indexOf(user.id), 1)
-      return new Promise((resolve) => setTimeout(() => resolve(cloud), 3000))
+    DDMMYY: {
+      value: 'DD.MM.YY',
+      description: 'Renders "DD.MM.YY"'
+    },
+    DDMMYYYY: {
+      value: 'DD.MM.YYYY',
+      description: 'Renders "DD.MM.YYYY"'
     }
-  },
-
-  User: {
-    clouds: (user, args) => {
-      const usersIds = users.map(({id}) => id)
-      if (args.ids) {
-        args.ids = usersIds.filter((id) => args.ids.includes(id))
-      } else {
-        args.ids = usersIds
-      }
-      return getFilteredClouds(args)
-    },
-    createdAt: (user, args) => getFormatedTime(user.createdAt, args.timeFormat),
-    deletedAt: (user, args) => getFormatedTime(user.deletedAt, args.timeFormat)
-  },
-
-  Cloud: {
-    owner: (cloud) => users.find((user) => user.id === cloud.ownerId),
-    users: (cloud, args) => {
-      if (args.ids) {
-        args.ids = cloud.usersIds.filter((id) => args.ids.includes(id))
-      } else {
-        args.ids = cloud.usersIds
-      }
-      return getFilteredUsers(args)
-    },
-    createdAt: (cloud, args) => getFormatedTime(cloud.createdAt, args.timeFormat),
-    deletedAt: (cloud, args) => getFormatedTime(cloud.deletedAt, args.timeFormat)
   }
-}
+})
 
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
+const UserInput = new GraphQLInputObjectType({
+  name: 'UserInput',
+  description: 'Used to search for User',
+  fields: () => ({
+    ids: {
+      type: new GraphQLList(GraphQLInt),
+      description: 'Users unique identifications'
+    },
+    name: {
+      type: GraphQLString,
+      description: 'Search on both User firstName & lastName'
+    }
+  })
+})
+
+const User = new GraphQLObjectType({
+  name: 'User',
+  description: 'Registered User',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLInt),
+      description: 'User unique identification'
+    },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+
+    clouds: {
+      type: new GraphQLList(Cloud),
+      description: 'All the Clouds that this User is Joined',
+      resolve: (user, args) => {
+        const usersIds = users.map(({id}) => id)
+        if (args.ids) {
+          args.ids = usersIds.filter((id) => args.ids.includes(id))
+        } else {
+          args.ids = usersIds
+        }
+        return getFilteredClouds(args)
+      }
+    },
+
+    createdAt: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The exact time when the User registered',
+      args: {
+        timeFormat: {
+          type: TimeFormat,
+          defaultValue: 'MM/DD/YY'
+        }
+      },
+      resolve: (user, args) => getFormatedTime(user.createdAt, args.timeFormat)
+    },
+
+    deletedAt: {
+      type: GraphQLString,
+      description: 'No data ever get deleted from the DB - only marked with a time when it was requested to be deleted',
+      args: {
+        timeFormat: {
+          type: TimeFormat,
+          defaultValue: 'MM/DD/YY'
+        }
+      },
+      resolve: (user, args) => getFormatedTime(user.createdAt, args.timeFormat)
+    }
+  })
+})
+
+const Cloud = new GraphQLObjectType({
+  name: 'Cloud',
+  description: 'A collection of Users with common goal',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLInt),
+      description: 'Cloud unique identification'
+    },
+    name: { type: GraphQLString },
+
+    owner: {
+      type: new GraphQLNonNull(User),
+      description: 'The one and only User that created the Cloud and has permissions over it',
+      resolve: (cloud) => users.find((user) => user.id === cloud.ownerId)
+    },
+
+    users: {
+      type: new GraphQLList(User),
+      description: 'All Users Joined to this Cloud',
+      resolve: (cloud, args) => {
+        if (args.ids) {
+          args.ids = cloud.usersIds.filter((id) => args.ids.includes(id))
+        } else {
+          args.ids = cloud.usersIds
+        }
+        return getFilteredUsers(args)
+      }
+    },
+
+    createdAt: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The exact time when the owner User created the Cloud',
+      args: {
+        timeFormat: {
+          type: TimeFormat,
+          defaultValue: 'MM/DD/YY'
+        }
+      },
+      resolve: (user, args) => getFormatedTime(user.createdAt, args.timeFormat)
+    },
+
+    deletedAt: {
+      type: GraphQLString,
+      description: 'No data ever get deleted from the DB - only marked with a time when it was requested to be deleted',
+      args: {
+        timeFormat: {
+          type: TimeFormat,
+          defaultValue: 'MM/DD/YY'
+        }
+      },
+      resolve: (user, args) => getFormatedTime(user.createdAt, args.timeFormat)
+    }
+  })
+})
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: 'All methods you can use to get data',
+    fields: {
+      users: {
+        type: new GraphQLList(User),
+        args: {
+          ids: {
+            type: new GraphQLList(GraphQLInt),
+            description: 'Users unique identifications'
+          },
+          name: {
+            type: GraphQLString,
+            description: 'User firstName or lastName includes this name'
+          }
+        },
+        resolve: (source, args) => getFilteredUsers(args)
+      },
+
+      clouds: {
+        type: new GraphQLList(Cloud),
+        args: {
+          ids: {
+            type: new GraphQLList(GraphQLInt),
+            description: 'Clouds unique identifications'
+          },
+          name: { type: GraphQLString },
+          user: {
+            type: UserInput,
+            description: 'Users that are joined in this Cloud'
+          }
+        },
+        resolve: (source, args) => getFilteredClouds(args)
+      }
+    }
+  }),
+
+  mutation: new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'All ways you can Save data to the BE',
+    fields: {
+      joinCloud: {
+        type: Cloud,
+        description: 'Use when a User would like to Join a Cloud',
+        args: {
+          userId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'User id to be added in the Cloud'
+          },
+          cloudId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'Cloud id where the User will be added'
+          }
+        },
+        resolve: (source, { userId, cloudId }) => {
+          const user = users.find((user) => user.id === userId)
+          if (!user) {
+            throw new Error(`Couldn't find User with id "${userId}"`)
+          }
+
+          const cloud = clouds.find((cloud) => cloud.id === cloudId)
+          if (!cloud) {
+            throw new Error(`Couldn't find Cloud with id "${cloudId}"`)
+          }
+
+          if (cloud.usersIds.includes(userId)) {
+            throw new Error(`User with id "${userId}" already joined Cloud with id "${cloudId}"`)
+          }
+
+          cloud.usersIds.push(userId)
+          return new Promise((resolve) => setTimeout(() => resolve(cloud), 3000))
+        }
+      },
+
+      leaveCloud: {
+        type: Cloud,
+        description: 'Use when a User would like to Leave a Cloud',
+        args: {
+          userId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'User id to be removed from the Cloud'
+          },
+          cloudId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'Cloud id where the User will be removed'
+          }
+        },
+        resolve: (source, { userId, cloudId }) => {
+          const user = users.find((user) => user.id === userId)
+          if (!user) {
+            throw new Error(`Couldn't find User with id "${userId}"`)
+          }
+
+          const cloud = clouds.find((cloud) => cloud.id === cloudId)
+          if (!cloud) {
+            throw new Error(`Couldn't find Cloud with id "${cloudId}"`)
+          }
+
+          if (!cloud.usersIds.includes(userId)) {
+            throw new Error(`User with id "${userId}" is not in Cloud with id "${cloudId}"`)
+          }
+
+          cloud.usersIds.splice(cloud.usersIds.indexOf(userId), 1)
+          return new Promise((resolve) => setTimeout(() => resolve(cloud), 3000))
+        }
+      },
+
+      loggedUserLeaveCloud: {
+        type: Cloud,
+        description: 'Use when the User from JWT would like to Leave a Cloud',
+        args: {
+          cloudId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'Cloud id where the User will be removed'
+          }
+        },
+        resolve: (source, { cloudId }, { token }) => {
+          if (!token) {
+            throw new Error(`Couldn't get the Authorization token`)
+          }
+
+          if (token.error) {
+            throw new Error(`Couldn't get the Authorization token - ${token.error}`)
+          }
+
+          const { user } = token
+          if (!user ||
+              !user.id
+            ) {
+            throw new Error(`User in the JWT have no "id" property`)
+          }
+
+          const cloud = clouds.find((cloud) => cloud.id === cloudId)
+          if (!cloud) {
+            throw new Error(`Couldn't find Cloud with id "${cloudId}"`)
+          }
+
+          if (!cloud.usersIds.includes(user.id)) {
+            throw new Error(`User with id "${user.id}" is not in Cloud with id "${cloudId}"`)
+          }
+
+          cloud.usersIds.splice(cloud.usersIds.indexOf(user.id), 1)
+          return new Promise((resolve) => setTimeout(() => resolve(cloud), 3000))
+        }
+      }
+    }
+  })
 })
 
 module.exports = schema
