fe627f96c0b34e0009e1d96fa02702373a78b164
Ivan Dimanov <ivan.dimanov@xogito.com>
2017-07-18 22:40:19 +0300

    BE - fully functionaly GraphQL server + logs + JWT + Data + Schema

--- back-end/services/graphql/index.js
+++ back-end/services/graphql/index.js
@@ -3,13 +3,16 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
+const { graphqlExpress } = require('graphql-server-express')
 const onFinished = require('on-finished')
+const { addErrorLoggingToSchema } = require('graphql-tools')
 
 const config = require('../../../config')
 const logger = require('../../../shared-modules/logger')
+const { expressJwtMiddleware } = require('../../../shared-modules/session')
 
 const schema = require('./schema')
+addErrorLoggingToSchema(schema, { log: (error) => logger.error(error) })
 
 const moduleParentDirectory = path.parse(__filename).dir
 const serviceName = path.parse(moduleParentDirectory).name
@@ -32,7 +35,32 @@ if (serviceConfig.trackRoutesLogs) {
   })
 }
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+/**
+ * Check for JWT Auth on every call
+ * but respect that there might be calls that are free of authentication.
+ * In order not to throw, we'll keep record if the user was successfully taken from the JWT
+ * or there was an error getting the JSON info
+ */
+app.use('*', (request, response, next) => {
+  expressJwtMiddleware({ userProperty: 'token' })(request, response, (error) => {
+    if (error) {
+      request.token = { error: error }
+    }
+    next()
+  })
+})
+
+app.use('/graphql', bodyParser.json(), (request, response, next) => {
+  graphqlExpress({
+    schema,
+    context: request
+  })(request, response, next)
+})
+
+if (config.environment !== 'production') {
+  app.use('/graphiql', (request, response, next) => {
+    response.sendFile(path.join(__dirname, '/graphiql.html'))
+  })
+}
 
 app.listen(port, host, () => logger.info(`Service ${serviceName} running on ${host}:${port}`))
